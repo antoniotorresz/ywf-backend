b9749983d68f418cac0ef127235032a74a106ead
antoniotorresz <antoniiotorresz1@gmail.com>
2024-09-10 13:26:27 -0600

    Initial structure

---
+++ src/main/java/com/ywf/ywf/YwfApplication.java
@@ -0,0 +1,13 @@
+package com.ywf.ywf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YwfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ywf/ywf/YwfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywf.ywf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
